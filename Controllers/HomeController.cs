0254ebce51c6aedcc18777aa695022a45ff60939
Đức Huỳnh Thanh <thanhduc4320@gmail.com>
2022-01-16 06:08:14 +0700

    UI for update, delete, query

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -129,16 +129,31 @@ namespace DAM.Controllers
       }
       
       if (Request.HasFormContentType) {
+        Type t = Type.GetType("DAM.Models." + className);
+        object obj = Activator.CreateInstance(t);
+        var methodInfo = t.GetMethod("setTableName");
+        methodInfo.Invoke(obj, new object[]{tableName});
+
         switch (Request.Form["handle"])
         {
           case "Update": 
             Console.WriteLine("Update");
+
+            foreach (var colName in colNames)
+            {  
+              TypeConverter typeConverter = TypeDescriptor.GetConverter(obj.GetType().GetProperty(colName).PropertyType);
+
+              if (Request.Form[colName] != "") {
+                obj.GetType().GetProperty(colName).SetValue(obj, typeConverter.ConvertFromString(Request.Form[colName]));
+              }
+            }
+
+            // Insert with savve method
+            methodInfo = t.GetMethod("update");
+            methodInfo.Invoke(obj, null);
             break;
           case "Insert": 
             Console.WriteLine("Insert");
-
-            Type t = Type.GetType("DAM.Models." + className);
-            object obj = Activator.CreateInstance(t);
           
             foreach (var colName in colNames)
             {  
@@ -149,15 +164,36 @@ namespace DAM.Controllers
               }
             }
 
-            var type = obj.GetType();
-            // Set tableName
-            var methodInfo = type.GetMethod("setTableName");
-            methodInfo.Invoke(obj, new object[]{tableName});
-
-            // Insert with savve method
-            methodInfo = type.GetMethod("save");
+            methodInfo = t.GetMethod("save");
             methodInfo.Invoke(obj, null);
             break;
+          case "Delete":
+            Console.WriteLine("Delete");
+
+            foreach (var colName in colNames)
+            {  
+              TypeConverter typeConverter = TypeDescriptor.GetConverter(obj.GetType().GetProperty(colName).PropertyType);
+
+              if (Request.Form[colName] != "") {
+                obj.GetType().GetProperty(colName).SetValue(obj, typeConverter.ConvertFromString(Request.Form[colName]));
+              }
+            }
+
+            methodInfo = t.GetMethod("delete");
+            methodInfo.Invoke(obj, null);
+            break;
+
+          case "Query":
+            Console.WriteLine("Query");
+            var result = db.Query(Request.Form["queryStr"]);
+
+            ViewBag.tableName = tableName;
+            ViewBag.colNames = colNames;
+            ViewBag.Data = result;
+
+            return View("TableInfo", ViewBag);
+            break;
+
           default:   
             Console.WriteLine("Invalid");
             break;
@@ -171,6 +207,14 @@ namespace DAM.Controllers
       ViewBag.Data = tableData;
 
       return View("TableInfo", ViewBag);
+    }
+
+    public IActionResult CloseConnection()
+    {
+      DBManager db = MySqLManager.GetInstance();
+      db.Close();
+       
+      return View("Index");
     }
   }
 }
